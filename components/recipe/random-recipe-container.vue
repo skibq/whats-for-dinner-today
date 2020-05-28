3636699aa1afcb08319ddb59758ca84217cf8be0
Michał Skiba <skiba.kontakt@gmail.com>
2020-05-28 04:47:28 +0200

    Display random recipe on the main page

---
+++ components/recipe/random-recipe-container.vue
@@ -0,0 +1,28 @@
+<template>
+  <Recipe :recipe="recipe" />
+</template>
+
+<script>
+import { randomRecipe } from 'spoonacular-api-library'
+import Recipe from '~/components/recipe/recipe'
+
+export default {
+  name: 'RandomRecipeContainer',
+  components: {
+    Recipe
+  },
+  data () {
+    return {
+      recipe: {}
+    }
+  },
+  created () {
+    randomRecipe.get().then(({ data }) => {
+      this.recipe = data.recipes[0]
+    })
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ components/recipe/recipe.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="recipe__container">
+    <div class="recipe">
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <h3 class="recipe__title">{{ recipe.title }}</h3>
+      <div class="recipe__instructions" v-html="recipe.instructions" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recipe',
+  props: {
+    recipe: {
+      type: Object,
+      default: () => {},
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '~/assets/styles/colours';
+
+  .recipe {
+    max-width: 1200px;
+    &__container {
+      display: flex;
+      justify-content: center;
+    }
+    &__title {
+      color: $main-color;
+      margin-bottom: 20px;
+      font-size: 38px;
+    }
+    &__instructions {
+      & ol {
+        list-style-type: none;
+        padding: 0;
+        & li {
+          margin-bottom: 5px;
+        }
+      }
+      padding: 0;
+    }
+  }
+</style>
